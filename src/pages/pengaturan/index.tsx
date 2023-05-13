1b973aba913c8543d5ba601936affab0146dbda0
E41211478 <e41211478@student.polije.ac.id>
2023-05-13 19:12:30 +0700

    input number

--- src/pages/pengaturan/index.tsx
+++ src/pages/pengaturan/index.tsx
@@ -1,60 +1,65 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
 import Link from 'next/link'
+import Navbar from '../components/Navbar';
 
 const Pengaturan = () => {
     return (
-        <div>
-            <div className="ml-80">
-                <Link
-                    href="/pengaturan/ruang"
-                >
-                    <button
-                        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-                    >
-                        ruang
-                    </button>
-                </Link>
-                <Link
-                    href="/pengaturan/sesi"
-                >
-                    <button
-                        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-                    >
-                        sesi
-                    </button>
-                </Link>
-                <Link
-                    href="/pengaturan/mapel"
-                >
-                    <button
-                        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-                    >
-                        mapel
-                    </button>
-                </Link>
-                <Link
-                    href="/pengaturan/kelas"
-                >
-                    <button
-                        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-                    >
-                        kelas
-                    </button>
-                </Link>
-                <Link
-                    href="/pengaturan/program"
-                >
-                    <button
-                        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-                    >
-                        program
-                    </button>
-                </Link>
-            </div>
+        <div className="flex flex-row">
             <Sidebar />
+
+            <div className="ml-10 w-full">
+                <Navbar />
+                <div className="ml-80">
+                    <Link
+                        href="/pengaturan/ruang"
+                    >
+                        <button
+                            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+                        >
+                            ruang
+                        </button>
+                    </Link>
+                    <Link
+                        href="/pengaturan/sesi"
+                    >
+                        <button
+                            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+                        >
+                            sesi
+                        </button>
+                    </Link>
+                    <Link
+                        href="/pengaturan/mapel"
+                    >
+                        <button
+                            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+                        >
+                            mapel
+                        </button>
+                    </Link>
+                    <Link
+                        href="/pengaturan/kelas"
+                    >
+                        <button
+                            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+                        >
+                            kelas
+                        </button>
+                    </Link>
+                    <Link
+                        href="/pengaturan/program"
+                    >
+                        <button
+                            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+                        >
+                            program
+                        </button>
+                    </Link>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Pengaturan
