805ec0be4163ba820d84490677c5885aa517fb97
E41211478 <e41211478@student.polije.ac.id>
2023-05-13 21:01:17 +0700

    get

--- src/pages/pengaturan/program/index.tsx
+++ src/pages/pengaturan/program/index.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link'
-import React from 'react'
-import { GetServerSideProps } from 'next';
-import prisma from '@/libs/prismadb';
+import React, { FC } from 'react'
+import { useEffect, useState } from 'react';
+import useSWR from 'swr';
+import fetcher from '@/libs/fetcher';
+
 
 interface Program {
     kelas: any;
@@ -19,49 +21,81 @@ interface Props {
 }
 
 
-const Program: React.FC<Props> = ({ program }) => {
+const Program: FC<Props> = () => {
+
+    const { data: program, error } = useSWR<Program[]>('/api/program', fetcher, {});
+
+    const [selectedProgram, setSelectedProgram] = useState<Program | null>(null);
+
+    useEffect(() => {
+        if (error) {
+
+        }
+    }, [error]);
+
+    const onClose = () => {
+        setSelectedProgram(null);
+    };
+
+    if (error) {
+        return <p>Error loading program.</p>;
+    }
 
     return (
         <div>
             <h1 className="font-bold text-4xl my-10">List Program</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nama Program</th>
-                        <th>Level</th>
-                        <th>Tipe</th>
-                        <th>Kelas</th>
-                        <th>Created At</th>
-                        <th>Updated At</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {program.map((item) => (
-                        <tr key={item.id}>
-                            <td>{item.id}</td>
-                            <td>{item.nama_program}</td>
-                            <td>{item.level}</td>
-                            <td>{item.tipe}</td>
-                            <td>{item.kelas.nama_kelas}</td>
-                            <td>{item.createdAt.toString()}</td>
-                            <td>{item.updatedAt.toString()}</td>
-                            <td>
-                                <Link
-                                    href={`/pengaturan/program/edit/${item.id}`}
-                                >
-                                    <button
-                                        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-                                    >
-                                        edit
-                                    </button>
-                                </Link>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            {
+                program ? (
+                    <>
+                        {program.length === 0 ? (
+                            <p>No program found.</p>
+                        ) : (
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Nama Program</th>
+                                        <th>Level</th>
+                                        <th>Tipe</th>
+                                        <th>Kelas</th>
+                                        <th>Created At</th>
+                                        <th>Updated At</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {program.map((item) => (
+                                        <tr key={item.id}>
+                                            <td>{item.id}</td>
+                                            <td>{item.nama_program}</td>
+                                            <td>{item.level}</td>
+                                            <td>{item.tipe}</td>
+                                            <td>{item.kelas.nama_kelas}</td>
+                                            <td>{item.createdAt.toString()}</td>
+                                            <td>{item.updatedAt.toString()}</td>
+                                            <td>
+                                                <Link
+                                                    href={`/pengaturan/program/edit/${item.id}`}
+                                                >
+                                                    <button
+                                                        className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+                                                    >
+                                                        edit
+                                                    </button>
+                                                </Link>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        )}
+                    </>
+                ) : (
+                    <p>Loading...</p>
+                )
+
+
+            }
             <Link
                 href="/pengaturan"
             >
@@ -74,37 +108,4 @@ const Program: React.FC<Props> = ({ program }) => {
         </div>
     )
 }
-
-export const getServerSideProps: GetServerSideProps = async () => {
-
-    // buatjoin dari table program dan kelas
-    const program = await prisma.program.findMany({
-        select: {
-            id: true,
-            nama_program: true,
-            tipe: true,
-            level: true,
-            kelas: {
-                select: {
-                    nama_kelas: true
-                }
-            },
-            createdAt: true,
-            updatedAt: true
-        }
-    });
-
-    const serializedMapel = program.map((item) => ({
-        ...item,
-        createdAt: item.createdAt.toString(),
-        updatedAt: item.updatedAt.toString()
-    }));
-
-    return {
-        props: {
-            program: serializedMapel
-        }
-    }
-}
-
 export default Program
