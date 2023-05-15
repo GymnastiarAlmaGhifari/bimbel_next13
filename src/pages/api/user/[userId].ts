5813657b308765904fbba3d770886e6fa9f83717
YogaNovvaindra <yoga.november2000@gmail.com>
2023-05-15 17:34:26 +0700

    user univ

--- src/pages/api/user/[userId].ts
+++ src/pages/api/user/[userId].ts
@@ -1,5 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/libs/prismadb";
+import fs from "fs/promises";
+import path from "path";
+import bcrypt from "bcrypt";
 
 //used edit with parameter userid
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -25,12 +28,50 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       res.status(500).json({ message: "Error loading user" });
     }
   } else if (req.method === "PUT") {
+
+    const directory = path.join(process.cwd(), "public", "img", "user");
+    const filenameWithoutExt = userId;
+
+    let filePath;
+    fs.readdir(directory)
+      .then((files) => {
+        console.log("Filenames in directory:", files); // log the filenames
+        const foundFile = files.find((file) => path.parse(file).name === filenameWithoutExt);
+        if (foundFile) {
+          filePath = path.join(foundFile);
+          console.log("Found file:", filePath);
+          return filePath.toString();
+        } else {
+          const foundFileWithExt = files.find((file) => file.startsWith(filenameWithoutExt + "."));
+          if (foundFileWithExt) {
+            filePath = path.join(foundFileWithExt);
+            console.log("Found file:", filePath);
+            return filePath.toString();
+          } else {
+            console.log("File not found");
+          }
+        }
+      })
+      .catch((err) => console.error(err));
+
+      const { name, email, role, password, nomor_telepon, alamat, image, universitas, mapel_id } = req.body;
+      
     try {
       const user = await prisma.user.update({
         where: { id: userId },
-        data: { ...req.body },
+        data: {
+          name: name,
+          email: email,
+//          password: await bcrypt.hash(password, 10),
+          role: role,
+          nomor_telepon: nomor_telepon,
+          alamat: alamat,
+          image: filePath,
+          universitas: universitas,
+          mapel_id: mapel_id,
+        },
       });
-
+      console.log("image", filePath);
       res.status(200).json(user);
     } catch (error) {
       console.error(error);
