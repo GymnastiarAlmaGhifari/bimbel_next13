03e8b4d8bf59f34cd4f242f5a80b4cafde42f61d
E41211478 <e41211478@student.polije.ac.id>
2023-05-17 19:17:41 +0700

    siswa

--- src/pages/siswa/index.tsx
+++ src/pages/siswa/index.tsx
@@ -1,10 +1,63 @@
-import React from "react";
+import React, { FC, useEffect, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
 import HeadTable from "../components/HeadTable";
 import CardSiswa from "../components/card/CardSiswa";
+import useSWR from "swr";
+import fetcher from "@/libs/fetcher";
+import { ModalDetail } from "@/pages/components/Modal";
+import { useSession } from "next-auth/react";
 
-const Siswa = () => {
+
+interface Siswa {
+  id: string;
+  nama: string;
+  email: string;
+  nomor_telepon: string;
+  alamat: string;
+  sekolah: string;
+  hp_ortu: string;
+  image: string;
+  program: any;
+  program_id: string;
+  tipe: string;
+  level: string;
+  kelas: any;
+  kelas_id: string;
+  nama_kelas: string;
+}
+
+const Siswa: FC<Siswa> = () => {
+  const { data: session, status } = useSession();
+
+  const { data: siswa, error } = useSWR<Siswa[]>("/api/siswa", fetcher, {});
+
+  // selectedit dan select delete
+
+  const [selectedEdit, setSelectedDelete] = useState<Siswa | null>(null);
+
+  const [selectedDelete, setSelectedEdit] = useState<Siswa | null>(null);
+
+  const [showCreate, setShowCreate] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShowSuccess(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [showSuccess]);
+
+  const backSiswa = () => {
+    setSelectedEdit(null);
+    setSelectedDelete(null);
+  };
+
+
+
   return (
     <div className="flex flex-row h-screen">
       <Sidebar />
@@ -14,13 +67,49 @@ const Siswa = () => {
           <div className="flex flex-col h-full bg-Neutral-100 py-4 gap-4 rounded-lg overflow-auto">
             <HeadTable label="Siswa" />
             <div className="flex flex-col rounded-bl-lg rounded-br-lg p-4 gap-4 overflow-y-auto scrollbar">
-              <CardSiswa
-                tipe="Private"
-                kelas="3 SMP"
-                kelompok="Reguler"
-                nama_siswa="Agimul Karim"
-                status="Aktif"
-              />
+
+              {
+                siswa ? (
+                  <>
+                    {siswa.length === 0 ? (
+                      <div className="flex flex-col justify-center items-center">
+                        <p className="text-2xl font-bold text-Neutral-600">Data Kosong</p>
+                        <p className="text-Neutral-500">Silahkan tambahkan data siswa</p>
+                      </div>
+                    ) : (
+                      <>
+                        {siswa.map((siswa) => (
+                          <CardSiswa
+                            key={siswa.id}
+                            tipe={siswa.tipe}
+                            kelas={siswa.kelas.nama_kelas}
+                            kelompok={siswa.level}
+                            nama_siswa={siswa.nama}
+                            status="Aktif"
+                            onEdit={() => setSelectedEdit(siswa)}
+                            onDelete={() => setSelectedDelete(siswa)}
+                          />
+                        ))}
+                      </>
+                    )}
+                  </>
+                ) : (
+                  <>
+                    {error ? (
+                      <div className="flex flex-col justify-center items-center">
+                        <p className="text-2xl font-bold text-Neutral-600">Data Kosong</p>
+                        <p className="text-Neutral-500">Silahkan tambahkan data siswa</p>
+                      </div>
+                    ) : (
+                      <div className="flex flex-col justify-center items-center">
+                        <p className="text-2xl font-bold text-Neutral-600">Loading...</p>
+                      </div>
+                    )
+
+                    }
+                  </>
+                )
+              }
             </div>
           </div>
         </div>
