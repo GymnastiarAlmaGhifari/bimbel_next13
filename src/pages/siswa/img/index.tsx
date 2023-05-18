25d8d6f516515d616e9a57f378385baaa2fce11d
YogaNovvaindra <yoga.november2000@gmail.com>
2023-05-18 17:54:01 +0700

    img

---
+++ src/pages/siswa/img/index.tsx
@@ -0,0 +1,7 @@
+// import Image from "next/image";
+
+
+// export default function Avatar() {
+//     console.log("Avatar");
+//     return <Image src="/img/siswa/*" width={1080} height={1080} alt="clhhq66eo0000pfx001b7c9o5.jpg" />
+// }
