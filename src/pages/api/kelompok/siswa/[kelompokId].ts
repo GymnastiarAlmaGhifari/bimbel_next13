fe8053cba5855a5aaf3f8ebd42eae222855f0026
E41211478 <e41211478@student.polije.ac.id>
2023-05-22 21:37:47 +0700

    siswa kelompok checkbox agim turu

--- src/pages/api/kelompok/siswa/[kelompokId].ts
+++ src/pages/api/kelompok/siswa/[kelompokId].ts
@@ -8,67 +8,58 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     kelompokId = kelompokId[0];
   }
 
-  
-  
   if (req.method === "GET") {
     try {
       const anggota = await prisma.siswa.findMany({
         where: { kelompok_id: kelompokId },
       });
-      
       res.status(200).json(anggota);
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Error loading anggota" });
     }
-    
   } else if (req.method === "PUT") {
     const siswabody = req.body.data;
 
-      try {
-        const getsiswa = await prisma.siswa.findMany({
-          where: { kelompok_id: kelompokId },
-        });
+    try {
+      const getsiswa = await prisma.siswa.findMany({
+        where: { kelompok_id: kelompokId },
+      });
 
-        for (let i = 0; i < getsiswa.length; i++) {
-          let found = false;
-          for (let j = 0; j < siswabody.length; j++) {
-            if (getsiswa[i].id === siswabody[j]) {
-              found = true;
-              break;
-            }
+      let allSame = true;
+
+      for (let i = 0; i < getsiswa.length; i++) {
+        let found = false;
+        for (let j = 0; j < siswabody.length; j++) {
+          if (getsiswa[i].id === siswabody[j]) {
+            found = true;
+            break;
           }
-          if (!found) {
-            try {
-              const siswa = await prisma.siswa.update({
-                where: { id: getsiswa[i].id },
-                data: { kelompok_id: null },
-              });
-
-              res.status(200).json({message: "success"});
-            }
-            catch (error) {
-              console.error(error);
-              res.status(200).json({ message: "Tidak ada siswa yang dihapus dari kelompok" });
-            }
+        }
+        if (!found) {
+          allSame = false;
+          try {
+            const siswa = await prisma.siswa.update({
+              where: { id: getsiswa[i].id },
+              data: { kelompok_id: null },
+            });
+          } catch (error) {
+            console.error(error);
+            res.status(200).json({
+              message: "Tidak ada siswa yang dihapus dari kelompok",
+            });
           }
         }
       }
-      catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Error updating siswa" });
-      }
 
-    // try {
-    //   const siswa = await prisma.siswa.update({
-    //     where: { id: siswaId },
-    //     data: { kelompok_id: kelompokId },
-    //   });
-    //   res.status(200).json(siswa);
-    // }
-    // catch (error) {
-    //   console.error(error);
-    //   res.status(500).json({ message: "Error updating siswa" });
-    // }
+      if (allSame) {
+        res.status(200).json({ message: "All data is the same" });
+      } else {
+        res.status(200).json({ message: "Success" });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Error updating siswa" });
+    }
   }
 }
--- src/pages/api/kelompok/siswanull/[kelompokId].ts
+++ src/pages/api/kelompok/siswanull/[kelompokId].ts
@@ -2,24 +2,49 @@ import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/libs/prismadb";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    let kelompokId = req.query.kelompokId;
+  let kelompokId = req.query.kelompokId;
 
-    if (Array.isArray(kelompokId)) {
-        kelompokId = kelompokId[0];
+  if (Array.isArray(kelompokId)) {
+    kelompokId = kelompokId[0];
+  }
+
+  if (req.method === "GET") {
+    try {
+      const anggota = await prisma.siswa.findMany({
+        where: { kelompok_id: kelompokId },
+      });
+
+      res.status(200).json(anggota);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Error loading anggota" });
+    }
+  } else if (req.method === "PUT") {
+    const siswabody = req.body.data;
+
+    if (!Array.isArray(siswabody)) {
+      res.status(200).json({ message: "Invalid siswabody data" });
+      return;
     }
 
-    const siswaId = req.body.siswaId;
-
-if (req.method === "GET") {
-        try {
-            const anggota = await prisma.siswa.findMany({
-                where: { kelompok_id: kelompokId },
-            });
-
-            res.status(200).json(anggota);
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ message: "Error loading anggota" });
+    try {
+      const updatePromises = siswabody.map((item: any) => {
+        if (item === false) {
+          return Promise.resolve(); // Skip updating the false value
+        } else {
+          return prisma.siswa.update({
+            where: { id: item },
+            data: { kelompok_id: kelompokId as string },
+          });
         }
+      });
+
+      await Promise.all(updatePromises);
+
+      res.status(200).json({ message: "Success" });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Error updating siswa" });
     }
+  }
 }
