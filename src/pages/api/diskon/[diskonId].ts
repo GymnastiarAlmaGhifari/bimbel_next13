049be19f9b0d4344b24b1138be20138002b6b1b3
YogaNovvaindra <yoga.november2000@gmail.com>
2023-05-28 16:11:11 +0700

    api api api api

---
+++ src/pages/api/diskon/[diskonId].ts
@@ -0,0 +1,39 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/libs/prismadb";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const id = req.query.diskonId as string;
+
+    if (req.method === "GET") {
+        try {
+            const diskon = await prisma.diskon.findUnique({
+                where: {
+                    id: id,
+                },
+            });
+
+            res.status(200).json(diskon);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Error loading diskon" });
+        }
+    } else if (req.method === "POST") {
+        try {
+            const diskon = await prisma.diskon.update({
+                where: {
+                    id: id,
+                },
+                data: {
+                    nama_diskon: req.body.nama_diskon,
+                },
+            });
+
+            res.status(200).json(diskon);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Error updating diskon" });
+        }
+    } else {
+        res.status(405).json({ message: "Method not allowed" });
+    }
+}
