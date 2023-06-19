0f68360d10242b8500e2ae1f0d7a5418d6aa72a6
YogaNovvaindra <yoga.november2000@gmail.com>
2023-06-19 12:50:16 +0700

    usr img edit

--- src/pages/api/user/userimg/[userId].ts
+++ src/pages/api/user/userimg/[userId].ts
@@ -27,30 +27,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       res.status(500).json({ message: "Error loading user" });
     }
   } else if (req.method === "PUT") {
-    // const directory = path.join(process.cwd(), "public", "img", "user");
-    // const filenameWithoutExt = userId;
-
-    // let filePath;
-    // fs.promises.readdir(directory)
-    //   .then((files) => {
-    //     console.log("Filenames in directory:", files); // log the filenames
-    //     const foundFile = files.find((file) => path.parse(file).name === filenameWithoutExt);
-    //     if (foundFile) {
-    //       filePath = path.join(foundFile);
-    //       console.log("Found file:", filePath);
-    //       return filePath.toString();
-    //     } else {
-    //       const foundFileWithExt = files.find((file) => file.startsWith(filenameWithoutExt + "."));
-    //       if (foundFileWithExt) {
-    //         filePath = path.join(foundFileWithExt);
-    //         console.log("Found file:", filePath);
-    //         return filePath.toString();
-    //       } else {
-    //         console.log("File not found");
-    //       }
-    //     }
-    //   })
-    //   .catch((err) => console.error(err));
 
     try {
       const users = await prisma.user.findUnique({
@@ -67,89 +43,44 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       const imagePath = path.join(process.cwd(), "upload", "img", "user", img);
 
       try {
-        // Check if the image file exists
-        if (fs.existsSync(imagePath)) {
-          // Delete the image file
-          fs.unlinkSync(imagePath);
-          console.log({ message: "Image deleted successfully" });
 
-          const directory = path.join(process.cwd(), "upload", "img", "user");
-          const filenameWithoutExt = userId + "-" + users.random;
+        const directory = path.join(process.cwd(), "upload", "img", "user");
+        const filenameWithoutExt = userId;
+        console.log("Filename without extension:", filenameWithoutExt);
 
-          let filePath;
-          try {
-            const files = await fs.promises.readdir(directory);
-            const foundFile = files.find((file) => path.parse(file).name === filenameWithoutExt);
-            if (foundFile) {
-              filePath = path.join(foundFile);
+        let filePath;
+        try {
+          const files = await fs.promises.readdir(directory);
+          const foundFile = files.find((file) => path.parse(file).name === filenameWithoutExt);
+          if (foundFile) {
+            filePath = path.join(foundFile);
+            console.log("Found file:", filePath);
+          } else {
+            const foundFileWithExt = files.find((file) => file.startsWith(filenameWithoutExt + "."));
+            if (foundFileWithExt) {
+              filePath = path.join(foundFileWithExt);
               console.log("Found file:", filePath);
             } else {
-              const foundFileWithExt = files.find((file) => file.startsWith(filenameWithoutExt + "."));
-              if (foundFileWithExt) {
-                filePath = path.join(foundFileWithExt);
-                console.log("Found file:", filePath);
-              } else {
-                console.log("File not found");
-              }
+              console.log("File not found");
             }
-          } catch (err) {
-            console.error(err);
           }
+        } catch (err) {
+          console.error(err);
+        }
 
-          try {
-            const user = await prisma.user.update({
-              where: { id: userId },
-              data: {
-                image: filePath,
-              },
-            });
+        try {
+          const user = await prisma.user.update({
+            where: { id: userId },
+            data: {
+              image: filePath,
+            },
+          });
 
-            res.status(200).json(user);
-          } catch (error) {
-            console.error(error);
-            res.status(500).json({ message: "Error updating user" });
-          }
-        } else {
-          console.log({ message: "Image not found" });
-
-          const directory = path.join(process.cwd(), "upload", "img", "user");
-          const filenameWithoutExt = userId + "-" + users.random;
-          console.log("Filename without extension:", filenameWithoutExt);
-
-          let filePath;
-          try {
-            const files = await fs.promises.readdir(directory);
-            const foundFile = files.find((file) => path.parse(file).name === filenameWithoutExt);
-            if (foundFile) {
-              filePath = path.join(foundFile);
-              console.log("Found file:", filePath);
-            } else {
-              const foundFileWithExt = files.find((file) => file.startsWith(filenameWithoutExt + "."));
-              if (foundFileWithExt) {
-                filePath = path.join(foundFileWithExt);
-                console.log("Found file:", filePath);
-              } else {
-                console.log("File not found");
-              }
-            }
-          } catch (err) {
-            console.error(err);
-          }
-
-          try {
-            const user = await prisma.user.update({
-              where: { id: userId },
-              data: {
-                image: filePath,
-              },
-            });
-
-            console.log("image", filePath);
-            res.status(200).json(user);
-          } catch (error) {
-            console.error(error);
-            res.status(500).json({ message: "Error updating user" });
-          }
+          console.log("image", filePath);
+          res.status(200).json(user);
+        } catch (error) {
+          console.error(error);
+          res.status(500).json({ message: "Error updating user" });
         }
       } catch (error) {
         console.error(error);
